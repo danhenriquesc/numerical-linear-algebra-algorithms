04112e8aba04a5ea77cee7459a4ad0ac08503bce
Daniel Henrique <daniel.intelie@gmail.com>
2017-07-11 02:10:24 -0300

    WIP - Gram-Schmidt QR Decomposition - FIXED

--- Rel3/gram-schmidt-qr.cpp
+++ Rel3/gram-schmidt-qr.cpp
@@ -18,40 +18,65 @@ const clock_t begin_time = clock();
 
 
 int main(){
-	double_matrix V, U, Q, R;
+	double_matrix V_t, V, U, Q, R, Ql, Rl, D, D_t;
 	double el;
 
 	//reading input
 	cin >> N;
 
-	for(int i = 0; i < N; i++){
+	for(int i = 0; i < N; i++)
+	{
 		double_vector row;
+		V_t.push_back(row);
 		V.push_back(row);
 		U.push_back(row);
 		Q.push_back(row);
 		R.push_back(row);
+		Ql.push_back(row);
+		Rl.push_back(row);
+		D.push_back(row);
+		D_t.push_back(row);
 
-		for(int j = 0; j < N; j++){
+		for(int j = 0; j < N; j++)
+		{
 			cin >> el;
-			V[i].push_back(el);
+			V_t[i].push_back(el);
 		}
 
-		for(int j = 0; j < N; j++){
+		for(int j = 0; j < N; j++)
+		{
 			Q[i].push_back(0);
+			Ql[i].push_back(0);
 			R[i].push_back(0);
+			Rl[i].push_back(0);
+			D[i].push_back(0);
+			D_t[i].push_back(0);
+			V[i].push_back(0);
 		}
 	}
 
+	for(int i = 0; i < N; i++)
+	{
+		for(int j = 0; j < N; j++)
+		{
+			V[j][i] = V_t[i][j];
+		}
+	}
+	
+	printMatrix(V_t, "Original T Matrix");
 	printMatrix(V, "Original Matrix");
 
 	//Gram-Schmidt Process
 	U[0] = V[0];
 
-	for(int i = 1; i < N; i++){
-		for(int j = 0; j < N; j++){
+	for(int i = 1; i < N; i++)
+	{
+		for(int j = 0; j < N; j++)
+		{
 			el = V[i][j];
 
-			for(int t=0;t<i;t++){
+			for(int t=0;t<i;t++)
+			{
 				el -= projection(U[t],V[i])[j];
 			}
 
@@ -61,7 +86,23 @@ int main(){
 
 	printMatrix(U, "Orthogonal Matrix");
 
-	Q = U;
+	for(int i = 0; i < N; i++)
+	{
+		for(int j = 0; j < N; j++)
+		{
+			Ql[j][i] = U[i][j];
+		}
+	}
+
+	printMatrix(Ql, "Matrix Q'");
+
+	for(int i = 0; i < N; i++)
+	{
+		D[i][i] = 1/norm(U[i]);
+	}
+
+	printMatrix(D, "Matrix D");
+
 
 	for(int n = 0; n < N; n++)
 	{
@@ -69,30 +110,57 @@ int main(){
 		{
 			if(n > m)
 			{
-				R[n][m] = 0;
+				Rl[n][m] = 0;
 			}
 			else
 			{
-				R[n][m] = innerProductSpace(V[n],U[m])/innerProductSpace(U[m],U[m]);
+				Rl[n][m] = innerProductSpace(V[m],U[n]);
 			}
 		}
 	}
 
-	printMatrix(Q, "Matrix Q'");
-	printMatrix(R, "Matrix R'");
+	printMatrix(Rl, "Matrix R'");
 
-	
-	for(int n = 0; n < N; n++)
+	for(int i = 0; i < N; i++)
 	{
-		double currentNorm = norm(Q[n]);
-		for(int m = 0; m < N; m++)
+		for(int j = 0; j < N; j++)
 		{
-			Q[n][m] /= currentNorm;
-			R[n][m] /= currentNorm;
+			D_t[j][i] = D[i][j];
+		}
+	}
+
+	for(int i = 0; i < N; i++)
+	{
+		for(int j = 0; j < N; j++)
+		{
+			double sum = 0;
+
+			for(int k = 0; k < N; k++)
+			{
+				sum += Ql[i][k] * D_t[k][j];
+			}
+
+			Q[i][j] = sum;
 		}
 	}
 
 	printMatrix(Q, "Matrix Q");
+
+	for(int i = 0; i < N; i++)
+	{
+		for(int j = 0; j < N; j++)
+		{
+			double sum = 0;
+
+			for(int k = 0; k < N; k++)
+			{
+				sum += D[i][k] * Rl[k][j];
+			}
+
+			R[i][j] = sum;
+		}
+	}
+	
 	printMatrix(R, "Matrix R");
 	
 	cout << "Total Time: " << fixed << float( clock () - begin_time ) /  CLOCKS_PER_SEC << " s" << endl;
@@ -103,7 +171,8 @@ int main(){
 
 double innerProductSpace(double_vector v, double_vector u){
 	double tot = 0;
-	for(int i = 0; i < N; i++){
+	for(int i = 0; i < N; i++)
+	{
 		tot += v[i]*u[i];
 	}
 	return tot;
@@ -115,7 +184,8 @@ double_vector projection(double_vector u, double_vector v){
 
 	double tmp_div = innerProductSpace(v,u)/innerProductSpace(u,u);
 
-	for(int i = 0; i<N; i++){
+	for(int i = 0; i<N; i++)
+	{
 		el = tmp_div*u[i];
 		p.push_back(el);
 	}
@@ -126,7 +196,8 @@ double_vector projection(double_vector u, double_vector v){
 double norm(double_vector u){
 	double sum = 0;
 
-	for(int i = 0; i < N; i++){
+	for(int i = 0; i < N; i++)
+	{
 		sum += pow(u[i], 2);
 	}
 
@@ -136,8 +207,10 @@ double norm(double_vector u){
 void printMatrix(double_matrix U, string title){
 	cout << title << endl;
 
-	for(int i = 0; i < N; i++){
-		for(int j = 0; j < N; j++){
+	for(int i = 0; i < N; i++)
+	{
+		for(int j = 0; j < N; j++)
+		{
 			cout << setw(15) << fixed << U[i][j] << " ";
 		}
 		cout << endl;
