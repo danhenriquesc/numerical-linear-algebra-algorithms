821f331d4818a613c4582ce14ce22473ad4a0ebd
Daniel Henrique <daniel.henrique@intelie.com.br>
2017-07-15 03:47:26 -0300

    ADDED Householder Reflection QR now solves equation systems

--- Rel3/householder-reflection-qr.cpp
+++ Rel3/householder-reflection-qr.cpp
@@ -1,3 +1,37 @@
+/*
+	Algorithm to solve Linear Equations Algorithms (Just for square matrix)
+	Householder Reflection QR Decomposition
+	Daniel Henrique (daniel.henrique.sc@gmail.com | daniel.henrique@ime.uerj.br) - 2017
+
+	Solve Ax = B(i..n)
+
+	Input:
+	N M
+	a11 a12 a13 ... a1n b11 b12 b13 ... b1m
+	a21 a22 a23 ... a2n b21 b22 b23 ... b2m
+	 .   .   .       .   .   .   .       .
+	an1 an2 an3 ... ann bn1 bn2 bn3 ... bnm 
+
+	Example:
+	3 3
+	1 2 3 10 11 12
+	4 5 6 13 14 15
+	7 8 9 16 17 18
+
+	Solves:
+	1x+2y+3z = 10
+	4x+5y+6z = 13
+	7x+8y+9z = 16
+
+	1x+2y+3z = 11
+	4x+5y+6z = 14
+	7x+8y+9z = 15
+
+	1x+2y+3z = 12
+	4x+5y+6z = 15
+	7x+8y+9z = 18
+*/
+
 #include <iostream>
 #include <vector>
 #include <iomanip>
@@ -11,6 +45,8 @@ typedef vector<double_vector> double_matrix;
 
 int N, M;
 void printMatrix(double_matrix, string);
+void printVector(double_vector, string);
+void printVector(double_matrix, int, string);
 double_matrix multiplyMatrix(double_matrix, double_matrix);
 double_matrix transposeMatriz(double_matrix);
 double_matrix identity(int);
@@ -23,21 +59,30 @@ double signal(double);
 const clock_t begin_time = clock();
 
 int main(){
-	double_matrix A, H, Q, R;
+	double_matrix A, H, Q, R, B, X;
 	double el;
 
-	// N lines and M columns matrix
+	// NxN matriz with M results
 	cin >> N >> M;
 
 	for(int i = 0; i < N; i++)
 	{
 		double_vector row;
 		A.push_back(row);
+		B.push_back(row);
+		X.push_back(row);
 		
-		for(int j = 0; j < M; j++)
+		for(int j = 0; j < N; j++)
 		{
 			cin >> el;
 			A[i].push_back(el);
+		}
+
+		for(int j=0; j < M; j++)
+		{
+			cin >> el;
+			B[i].push_back(el);
+			X[i].push_back(0);
 		}
 	}
 
@@ -48,7 +93,7 @@ int main(){
 	int i = 0;
 	while(true)
 	{	
-		if( (N-i < 2) || (M-i < 2) )
+		if(N-i < 2)
 		{
 			break;
 		}
@@ -120,12 +165,12 @@ int main(){
 	}
 
 	//Creating R by A
-	for(int i = 0; i < M; i++)
+	for(int i = 0; i < N; i++)
 	{
 		double_vector row;
 		R.push_back(row);
 
-		for(int j = 0; j < M; j++)
+		for(int j = 0; j < N; j++)
 		{
 			R[i].push_back(A[i][j]);
 		}
@@ -134,6 +179,41 @@ int main(){
 	printMatrix(Q, "Matrix Q");
 	printMatrix(R, "Matriz R");
 
+	double_matrix Q_t = transposeMatriz(Q);
+	//Calculating X values
+	for(int i = 0; i < M; i++)
+	{
+		cout << "CASE #" << (i+1) << ":" << endl << endl;
+		printVector(B, i, "B" + to_string(i+1) + ": ");
+
+		double_matrix B_Matrix;
+
+		for (int j = 0; j < N; j++)
+		{
+			double_vector row;
+			row.push_back(B[j][i]);
+			B_Matrix.push_back(row);
+		}
+
+		double_matrix Y = multiplyMatrix(Q_t, B_Matrix);
+		printVector(Y, 0, "Matrix Y (y = Q_t*B)");
+
+		//y = Rx - Finding out X
+		for(int l=N-1; l >= 0; l--)
+		{
+			double value = Y[l][0];
+
+			for(int c=l+1; c < N; c++)
+			{
+				value -= R[l][c]*X[i][c];
+			}
+
+			X[i][l] = value/R[l][l];
+		}
+
+		printVector(X[i],"Result (Rx = y)");
+	}
+	
 	cout << "Total Time: " << fixed << float( clock () - begin_time ) /  CLOCKS_PER_SEC << " s" << endl;
 
 	return 0;
@@ -154,6 +234,31 @@ void printMatrix(double_matrix U, string title)
 
 	cout << endl;
 }
+
+void printVector(double_vector U, string title)
+{
+	cout << title << endl;
+
+	for(int j = 0; j < U.size(); j++)
+	{
+		cout << (j+1) << ": " << setw(15) << fixed << U[j] << endl;
+	}
+
+	cout << endl;
+}
+
+void printVector(double_matrix U, int L, string title)
+{
+	cout << title << endl;
+
+	for(int j = 0; j < U.size(); j++)
+	{
+		cout << (j+1) << ": " << setw(15) << fixed << U[j][L] << endl;
+	}
+
+	cout << endl;
+}
+
 
 double_matrix multiplyMatrix(double_matrix A, double_matrix B)
 {
